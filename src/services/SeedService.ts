414ed653671cc38a2d1da902da71218eba3778c2
Asad Ali <asadcr@live.com>
2022-04-07 12:11:52 +0500

    cleanup

--- src/services/SeedService.ts
+++ src/services/SeedService.ts
@@ -8,7 +8,7 @@ class SeedService {
             { id: 2, label: "Gifts", },
             { id: 3, label: "Food", },
             { id: 4, label: "Going out", },
-            { id: 5, label: "Traveling", }
+            { id: 5, label: "Traveling", },
         ];
     };
 
