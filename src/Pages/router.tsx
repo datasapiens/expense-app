17f2dce13606da93c445d300c0198208eee53336
Asad Ali <asadcr@live.com>
2022-04-07 10:41:18 +0500

    adding router

---
+++ src/Pages/router.tsx
@@ -0,0 +1,32 @@
+import { Route, Routes } from "react-router-dom";
+import React, { Suspense} from "react";
+import ExtendedRouter from "../extendedRouter";
+import history from "../history";
+import MainLayout from "../components/MainLayout";
+import Loader from "../components/Loader";
+import Graphs from "./Graphs";
+import TransactionDetail from "./Transaction/TransactionDetail";
+import CategoryDetail from "./Category/CategoryDetail";
+import { Home } from './Home';
+
+class Router extends React.Component {
+    
+    render() {
+        return (
+            <ExtendedRouter history={history}>
+                <Suspense fallback={<Loader />}>
+                    <MainLayout>
+                        <Routes>
+                            <Route path="/graphs" element={<Graphs />} />
+                            <Route path="/" element={<Home />} />
+                            <Route path="/category-detail/:id" element={<CategoryDetail />} />
+                            <Route path="/transaction-details/:id" element={<TransactionDetail />} />
+                        </Routes>
+                    </MainLayout>
+                </Suspense>
+            </ExtendedRouter>
+        );
+    }
+}
+
+export default Router;
