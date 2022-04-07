08140a110822f05ab1f0f12d466e1b50d6060a0e
Asad Ali <asadcr@live.com>
2022-04-07 10:35:51 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Router from './Pages/router';
+
+function App() {
+  return (
+    <div className="App">
+    <Router />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MainLayout.tsx
@@ -0,0 +1,25 @@
+import { JsxElement } from 'typescript';
+import Menu from './Menu';
+import Nav from './Nav';
+
+ const MainLayout = (props: JsxElement) => {
+
+
+    return (
+      <>
+        <Nav />
+
+        <div className="container-fluid">
+          <div className="row">
+            <Menu />
+
+            <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                {props.children}
+            </main>
+          </div>
+        </div>
+      </>
+    );
+  }
+  export default MainLayout;
+
