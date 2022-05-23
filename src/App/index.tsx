bfdda63db8c6568303abd24bc1cb2cba39915b48
Torkinos <tornike.kapanaze@gmail.com>
2022-05-23 08:54:16 +0300

    develop: add eslint, moment and classnames plugins

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
-  return (
-    <div>
-      {}
-    </div>
-  );
+const App = () => {
+    return <div>{}</div>
 }
 
-export default App;
+export default App
