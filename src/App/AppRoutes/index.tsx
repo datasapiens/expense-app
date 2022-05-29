ae124c1ea2e67c1dac30bda32d3678c9a1a10a19
Torkinos <tornike.kapanaze@gmail.com>
2022-05-29 22:31:28 +0300

    develop: add index page and install deployment

--- src/App/AppRoutes/index.tsx
+++ src/App/AppRoutes/index.tsx
@@ -1,11 +1,22 @@
-import { FC } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { FC, useEffect } from 'react'
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom'
 import { GRAPHS_ROUTE, HOME_ROUTE, INDEX_ROUTE } from 'src/constants/routes'
 import Graphs from 'src/views/Graphs'
 import Home from 'src/views/Home'
 import Layout from 'src/App/Layout'
 
 const AppRoutes: FC = () => {
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (location.pathname === INDEX_ROUTE) {
+            navigate(HOME_ROUTE, {
+                replace: true,
+            })
+        }
+    }, [])
+
     return (
         <Routes>
             <Route element={<Layout />} path={INDEX_ROUTE}>
