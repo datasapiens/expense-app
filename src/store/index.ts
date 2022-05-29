e918544773d550d7a5d6ea1640e5595e2fa461e9
Torkinos <tornike.kapanaze@gmail.com>
2022-05-29 21:45:24 +0300

    develop: add local storage updating

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
-import transactions from './transactions'
-import categories from './categories'
+import { setCategoriesToLocalStorage } from 'src/localStorage/categories'
+import { setTransactionsToLocalStorage } from 'src/localStorage/transactions'
+import transactions, { selectTransactions } from './transactions'
+import categories, { selectCategories } from './categories'
 
 export const store = configureStore({
     reducer: {
         categories,
         transactions,
     },
+})
+
+store.subscribe(() => {
+    setCategoriesToLocalStorage(selectCategories(store.getState()))
+    setTransactionsToLocalStorage(selectTransactions(store.getState()))
 })
