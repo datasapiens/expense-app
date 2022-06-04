0b4290a55abf7676b4fda7a88ffd083e97e506b5
Theodore Obonye <ltobonye@gmail.com>
2022-06-04 07:25:02 +0200

    refactor: add chart & state for side nav

--- expense-app/src/pages/side-nav/SideNav.tsx
+++ expense-app/src/pages/side-nav/SideNav.tsx
@@ -2,16 +2,21 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChartLine, faHouse } from "@fortawesome/free-solid-svg-icons";
 import "./SideNav.scss";
+import { useState } from "react";
 
 const SideNav = () => {
+  const [toggleState, setToggleState] = useState(1);
+  const toggleTab = (index: number) => {
+    setToggleState(index);
+  };
   return (
     <>
       <nav className="sidebar">
-        <Link to={"/"} className="link">
+        <Link to={"/"} onClick={() => toggleTab(1)} className={toggleState === 1 ? "link active" : "link"}>
           <FontAwesomeIcon icon={faHouse} className="icon" />
           Home
         </Link>
-        <Link to={"/graphs"} className="link">
+        <Link to={"/graphs"} onClick={() => toggleTab(2)} className={toggleState === 2 ? "link active" : "link"}>
           <FontAwesomeIcon icon={faChartLine} className="icon" />
           Graphs
         </Link>
