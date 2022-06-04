99509e49b8166083702ee9afd08ca5f6bb5f996b
Theodore Obonye <ltobonye@gmail.com>
2022-06-04 07:14:37 +0200

    refactor: update tabs for graphs page

--- expense-app/src/components/Tabs/GraphTab.tsx
+++ expense-app/src/components/Tabs/GraphTab.tsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import './tab.scss';
 
 function GraphTab() {
-  return (
-    <div>GraphTab</div>
-  )
+    const [toggleState, setToggleState] = useState(1);
+   
+  
+    const toggleTab = (index: number) => {
+      setToggleState(index);
+    };
+  
+    return (
+      <div className="container">
+        <div className="bloc-tabs">
+          <button
+            className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(1)}
+          >
+           Income vs. Expenses
+          </button>
+          <button
+            className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(2)}
+          >
+           Income per category
+          </button>
+          <button
+            className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(3)}
+          >
+           Expenses per category
+          </button>
+          <button
+            className={toggleState === 4 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(4)}
+          >
+           Income vs. Expenses Daily
+          </button>
+        </div>
+  
+        <div className="content-tabs">
+          <div
+            className={toggleState === 1 ? "content  active-content" : "content"}
+          >
+            <div className="header-button"><h2>Income vs. Expenses</h2>
+           
+             </div>
+          </div>
+          <div
+            className={toggleState === 2 ? "content  active-content" : "content"}
+          >
+            <div className="header-button">
+              <h2>Income per category</h2> 
+            
+              </div>
+          </div>
+          <div
+            className={toggleState === 3 ? "content  active-content" : "content"}
+          >
+            <div className="header-button">
+              <h2>Expenses per category</h2> 
+            
+              </div>
+          </div>
+          <div
+            className={toggleState === 4 ? "content  active-content" : "content"}
+          >
+            <div className="header-button">
+              <h2>Income vs. Expenses Daily</h2> 
+            
+              </div>
+          </div>
+        </div>
+      </div>
+    )
 }
 
 export default GraphTab
