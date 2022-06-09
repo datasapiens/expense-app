40109c2b024f24348a5b8b649fc480f715440a2d
Theodore Obonye <ltobonye@gmail.com>
2022-06-09 19:12:21 +0200

    ci: update lockfile

--- expense-app/src/index.tsx
+++ expense-app/src/index.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.scss";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./state/state.module";
 
@@ -21,4 +20,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
