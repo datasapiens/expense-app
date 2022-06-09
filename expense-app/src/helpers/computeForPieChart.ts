c13e55457f538d55296ef4c41cefa92c9ca1e698
Theodore Obonye <ltobonye@gmail.com>
2022-06-09 19:38:37 +0200

    update charts

--- expense-app/src/helpers/computeForPieChart.ts
+++ expense-app/src/helpers/computeForPieChart.ts
@@ -1,57 +1,84 @@
 import { Transaction } from "../interfaces/transaction.interface";
 
 export const getIncome = (transactions: Transaction[]) => {
-    const income: number[] =[];
-    transactions.forEach((tx) => {
-      if (tx.amount) {
-        if (tx.amount > 0) {
-          income.push(tx.amount)
-        }
+  const income: number[] = [];
+  transactions.forEach((tx) => {
+    if (tx.amount) {
+      if (tx.amount > 0) {
+        income.push(tx.amount);
       }
-    });
+    }
+  });
   return income;
-}
+};
 export const getExpenses = (transactions: Transaction[]) => {
-    const expense: number[] = [];
-    transactions.forEach((tx) => {
-      if (tx.amount) {
-        if (tx.amount < 0) {
-          expense.push(tx.amount)
-        }
+  const expense: number[] = [];
+  transactions.forEach((tx) => {
+    if (tx.amount) {
+      if (tx.amount < 0) {
+        expense.push(tx.amount);
       }
-    });
-    return expense;
-}
+    }
+  });
+  return expense;
+};
 
 export const PieChartData = (transactions: Transaction[]) => {
-    const income = getIncome(transactions).reduce((a , b) => a + b);
-    const expense = getExpenses(transactions).reduce((a , b) => a + b);
+  if (transactions.length > 0) {
+    const income = getIncome(transactions).reduce((a, b) => a + b);
+    const expense = getExpenses(transactions).reduce((a, b) => a + b);
     return {
-        labels: ['Income', 'Expenses'],
-        datasets: [
-          {
-            label: '# of Votes',
-            data: [income, expense],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ]
-    }
-
-   
-  };
+      labels: ["Income", "Expenses"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [income, expense],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+  } else {
+    return {
+      labels: ["Income", "Expenses"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [0, 0],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+  }
+};
