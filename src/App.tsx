95e3500dfb7db3bf87defa176e28de50f20c23ed
Václav Beneš <pan.vaclav.benes@gmail.com>
2022-06-27 02:30:28 +0200

    back to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Charts } from './pages/Charts'
 import styles from './App.module.scss'
@@ -7,14 +7,14 @@ import { NavBar } from './components/NavBar/NavBar'
 function App() {
   return (
     <div className={styles.layout}>
-      <BrowserRouter basename="/expense-app">
+      <HashRouter>
         <NavBar />
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/charts" element={<Charts />} />
           <Route path="/" element={<Navigate to="/home" replace={true} />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   )
 }
