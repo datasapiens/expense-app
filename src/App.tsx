8af9e7abd20c35151b9e757ae8b7be88f8f0cce2
Václav Beneš <pan.vaclav.benes@gmail.com>
2022-06-27 02:13:57 +0200

    try gh-pages with browser roter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Charts } from './pages/Charts'
 import styles from './App.module.scss'
@@ -7,14 +7,14 @@ import { NavBar } from './components/NavBar/NavBar'
 function App() {
   return (
     <div className={styles.layout}>
-      <HashRouter>
+      <BrowserRouter>
         <NavBar />
         <Routes>
           <Route path="home" element={<Home />} />
           <Route path="charts" element={<Charts />} />
           <Route path="/" element={<Navigate to="/home" replace={true} />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   )
 }
