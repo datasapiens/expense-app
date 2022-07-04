cf7787c86edfa83eb4793d84bf8924d2e5b84b07
mdanko <michaldanko.sk@gmail.com>
2022-07-04 23:27:52 +0200

    fix: remove default transactions

--- src/features/transactions/transactionsSlice.ts
+++ src/features/transactions/transactionsSlice.ts
@@ -1,31 +1,7 @@
-import { Category, DEFAULT_CATEGORIES } from "../categories/categoriesSlice";
+import { Category } from "../categories/categoriesSlice";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "app/store";
 import { v4 as uuidv4 } from "uuid";
-
-const DEFAULT_TRANSACTIONS = [
-  {
-    id: uuidv4(),
-    label: "Hello",
-    date: "2020-11-11",
-    amount: 0,
-    category: DEFAULT_CATEGORIES[0].id,
-  },
-  {
-    id: uuidv4(),
-    label: "Bye",
-    date: "2021-10-10",
-    amount: 10,
-    category: DEFAULT_CATEGORIES[1].id,
-  },
-  {
-    id: uuidv4(),
-    label: "Bye",
-    date: "2022-06-06",
-    amount: -50,
-    category: DEFAULT_CATEGORIES[2].id,
-  },
-];
 
 export interface Transaction {
   id: string;
@@ -40,7 +16,7 @@ export interface TranstactionsState {
 }
 
 const initialState: TranstactionsState = {
-  transactions: DEFAULT_TRANSACTIONS,
+  transactions: [],
 };
 
 export const transactionsSlice = createSlice({
