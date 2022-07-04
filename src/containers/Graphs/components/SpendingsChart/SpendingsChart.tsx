e3fe364eb34c3670d8b150fd2969647b26129e4f
mdanko <michaldanko.sk@gmail.com>
2022-07-04 17:49:01 +0200

    feat: spendings chart

---
+++ src/containers/Graphs/components/SpendingsChart/SpendingsChart.tsx
@@ -0,0 +1,46 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+import { useAppSelector } from "app/hooks";
+import { selectTransactions } from "features/transactions/transactionsSlice";
+import { selectCategories } from "features/categories/categoriesSlice";
+import autocolors from "chartjs-plugin-autocolors";
+
+ChartJS.register(ArcElement, Tooltip, Legend, autocolors);
+
+const options = {
+  responsive: true,
+  plugins: {
+    autocolors: {
+      mode: "data",
+    },
+    legend: {
+      position: "top" as const,
+    },
+  },
+};
+
+export const SpendingsChart = () => {
+  const { items: transactions } = useAppSelector(selectTransactions);
+  const { items: categories } = useAppSelector(selectCategories);
+
+  const labels = categories.map(({ label }) => label);
+  const datasets = [
+    {
+      label: "Category",
+      data: categories.map(({ id }) =>
+        transactions
+          .filter(({ category, amount }) => category === id && amount < 0)
+          .reduce((acc, category) => acc + category.amount, 0),
+      ),
+      borderColor: new Array(categories.length).fill("black"),
+      borderWidth: 1,
+    },
+  ];
+
+  const data = {
+    labels,
+    datasets,
+  };
+
+  return <Pie options={options} data={data} />;
+};
