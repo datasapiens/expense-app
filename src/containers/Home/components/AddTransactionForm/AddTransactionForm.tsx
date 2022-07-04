5d61b02a8999de5e3aa5767d60189302ac6bb81e
mdanko <michaldanko.sk@gmail.com>
2022-07-04 17:47:58 +0200

    feat: transactions components

---
+++ src/containers/Home/components/AddTransactionForm/AddTransactionForm.tsx
@@ -0,0 +1,78 @@
+import { useAppDispatch, useAppSelector } from "app/hooks";
+import { FormInput } from "components/FormInput/FormInput";
+import { useForm } from "react-hook-form";
+import { selectCategories } from "features/categories/categoriesSlice";
+import { add, Transaction } from "features/transactions/transactionsSlice";
+import styles from "./AddTransactionForm.module.scss";
+
+interface FieldValues extends Omit<Transaction, "amount"> {
+  amount: string;
+}
+
+const ERROR_MESSAGE = "This field is required.";
+
+const minDate = "1900-01-01";
+const currentDate = new Date().toISOString().slice(0, 10);
+
+export const AddTransactionForm = () => {
+  const dispatch = useAppDispatch();
+  const { items: categories } = useAppSelector(selectCategories);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>();
+
+  const onSubmit = (data: FieldValues) => {
+    dispatch(
+      add({
+        ...data,
+        amount: parseFloat(data.amount),
+      }),
+    );
+    reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.addTransactionForm}>
+      <FormInput
+        {...register("label", { required: true, maxLength: 30 })}
+        error={errors.label ? ERROR_MESSAGE : ""}
+        label="Label"
+      />
+
+      <FormInput
+        {...register("date", { required: true })}
+        error={errors.date ? ERROR_MESSAGE : ""}
+        label="Date"
+        type="date"
+        defaultValue={currentDate}
+        min={minDate}
+        max={currentDate}
+      />
+
+      <FormInput
+        {...register("amount", { required: true, maxLength: 30 })}
+        error={errors.amount ? ERROR_MESSAGE : ""}
+        label="Amount (€)"
+        type="number"
+        step="any"
+      />
+
+      <label htmlFor="category" className={styles.inputLabel}>
+        Category
+      </label>
+      <select id="category" className={styles.select} {...register("category", { required: true })}>
+        {categories.map(({ id, label }) => (
+          <option key={id} value={id}>
+            {label}
+          </option>
+        ))}
+      </select>
+
+      <input type="submit" value="Submit" className={styles.submit} />
+    </form>
+  );
+};
---
+++ src/containers/Home/components/TransactionsTable/TransactionsTable.tsx
@@ -0,0 +1,46 @@
+import { useAppSelector } from "app/hooks";
+import { selectCategories } from "../../../../features/categories/categoriesSlice";
+import { selectTransactions } from "../../../../features/transactions/transactionsSlice";
+import styles from "./TransactionsTable.module.scss";
+
+const AMOUNT_LOCALE_OPTIONS = { style: "currency", currency: "EUR" };
+const NO_CATEGORY_LABEL = "No Category";
+
+export const TransactionsTable = () => {
+  const { items: transactions } = useAppSelector(selectTransactions);
+  const { items: categories } = useAppSelector(selectCategories);
+
+  return (
+    <table className={styles.transactionsTable}>
+      <thead>
+        <tr>
+          <th>Label</th>
+          <th>Date</th>
+          <th>Amount</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      {transactions && (
+        <tbody>
+          {transactions.map((row) => {
+            const category = categories.find(({ id }) => id === row.category);
+            const categoryLabel = category?.label ?? NO_CATEGORY_LABEL;
+            const amountColor =
+              row.amount > 0 ? styles.positive : row.amount < 0 ? styles.negative : undefined;
+
+            return (
+              <tr key={row.id}>
+                <td>{row.label}</td>
+                <td>{new Date(row.date).toLocaleDateString()}</td>
+                <td className={`${styles.cellAmount} ${amountColor}`}>
+                  {row.amount.toLocaleString(undefined, AMOUNT_LOCALE_OPTIONS)}
+                </td>
+                <td>{categoryLabel}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      )}
+    </table>
+  );
+};
